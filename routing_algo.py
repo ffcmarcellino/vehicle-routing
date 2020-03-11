5432144eed267a6a7591ee931c36a528efc33783
Fellipe Marcellino <ffcmarcellino@gmail.com>
2020-03-11 15:57:23 -0700

    First draft

---
+++ routing_algo.py
@@ -0,0 +1,26 @@
+# Objetos: veículos, origens, pedidos, clientes
+
+
+# inicializar estado (posicao veiculos, destino, estoques, demandas)
+
+#for t < T:
+
+# recarregar estoques
+
+# sample pedido
+
+# roteirizar
+    # roteirizar estoques moveis no meio da rota
+    # associar clientes a origem
+    # vrptw
+    # calcular destinos finais
+    # atualizar rotas
+    # atualizar destinos veículos
+
+# avançar veículos
+
+# entregar pedidos
+    ## atualizar status veiculo
+    ## atualizar status pedido
+
+# renderizar
